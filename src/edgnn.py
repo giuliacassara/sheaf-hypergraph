fix bug block_norm and add symetric normalisation

src/edgnn.py
@@ -24,7 +24,7 @@ from sheaf_builder import SheafBuilderDiag, SheafBuilderOrtho, SheafBuilderGener
 class EquivSetConv(nn.Module):
     def __init__(self, in_features, out_features, mlp1_layers=1, mlp2_layers=1,
         mlp3_layers=1, aggr='add', alpha=0.5, dropout=0., normalization='None', input_norm=False):
-        super().__init__()
+        super(EquivSetConv).__init__()
 
         if mlp1_layers > 0:
             self.W1 = MLP(in_features, out_features, out_features, mlp1_layers,
@@ -56,6 +56,7 @@ class EquivSetConv(nn.Module):
             self.W.reset_parameters()
 
     def forward(self, X, vertex, edges, X0, alpha):
+        pdb.set_trace()
         N = X.shape[-2]
 
         Xve = self.W1(X)[..., vertex, :] # [nnz, C]
@@ -332,10 +333,6 @@ class SheafEquivSetGNN(nn.Module):
         
         self.prediction_type = args.sheaf_pred_block
         self.dynamic_sheaf = args.dynamic_sheaf
-
-        # self.sheaf_lin = nn.ModuleList()
-        # self.transformer_layer = nn.ModuleList()
-        # self.transformer_lin_layer = nn.ModuleList()
         
         if sheaf_type == 'DiagEDGNN':
             ModelSheaf = SheafBuilderDiag

src/layers.py
@@ -27,6 +27,7 @@ from torch_geometric.typing import Adj, Size, OptTensor
 from typing import Optional
 import pdb
 
+import utils
 
 # This part is for PMA.
 # Modified from GATConv in pyg.
@@ -41,6 +42,54 @@ def zeros(tensor):
     if tensor is not None:
         tensor.data.fill_(0)
 
+def normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, d, norm_type='degree_norm'):
+    #this will return either D^-1/B^-1 or D^(-1/2)/B^-1
+    if norm_type == 'degree_norm':
+        #return D_inv and B_inv used to normalised the laplacian (/propagation)
+        #normalise using node/hyperedge degrees D_e and D_v in the paper
+        D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*d) 
+        D = 1.0 / D
+        D[D == float("inf")] = 0
+
+        B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*d)
+        B = 1.0 / B
+        B[B == float("inf")] = 0
+        return D, B
+
+    elif norm_type == 'sym_degree_norm':
+        #normalise using node/hyperedge degrees D_e and D_v in the paper
+        D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*d) 
+        D = D ** (-0.5)
+        D[D == float("inf")] = 0
+
+        B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*d)
+        B = 1.0 / B
+        B[B == float("inf")] = 0
+        return D, B
+
+    elif norm_type == 'block_norm':
+        #normalise using diag(HHT) and diag(HTH) <- this take into account the values predicted in H as oposed to 0/1 as in the degree
+        # this way of computing the normalisation tensor is only valid for diagonal sheaf
+        D = scatter_add(alpha*alpha, hyperedge_index[0], dim=0, dim_size=num_nodes*d)
+        D = 1.0 / D #can compute inverse like this because the matrix is diagonal
+        D[D == float("inf")] = 0
+
+        B = scatter_add(alpha*alpha, hyperedge_index[1], dim=0, dim_size=num_edges*d)
+        B = 1.0 / B #can compute inverse like this because the matrix is diagonal
+        B[B == float("inf")] = 0
+        return D, B
+    
+    elif norm_type == 'sym_block_norm':
+        #normalise using diag(HHT) and diag(HTH) <- this take into account the values predicted in H as oposed to 0/1 as in the degree
+        # this way of computing the normalisation tensor is only valid for diagonal sheaf
+        D = scatter_add(alpha*alpha, hyperedge_index[0], dim=0, dim_size=num_nodes*d)
+        D = D ** (-0.5) #can compute inverse like this because the matrix is diagonal
+        D[D == float("inf")] = 0
+
+        B = scatter_add(alpha*alpha, hyperedge_index[1], dim=0, dim_size=num_edges*d)
+        B = 1.0 / B  #can compute inverse like this because the matrix is diagonal
+        B[B == float("inf")] = 0
+        return D, B
 
 class PMA(MessagePassing):
     """
@@ -322,7 +371,7 @@ class HypergraphConv(MessagePassing):
     and Hypergraph Attention" <https://arxiv.org/abs/1901.08150>`_ paper
     """
     def __init__(self, in_channels, out_channels, use_attention=False, heads=1,
-                 concat=True, negative_slope=0.2, dropout=0, bias=True,
+                 concat=True, negative_slope=0.2, dropout=0, bias=True, residual = False,
                  **kwargs):
         kwargs.setdefault('aggr', 'add')
         super().__init__(flow='source_to_target', node_dim=0, **kwargs)
@@ -331,6 +380,7 @@ class HypergraphConv(MessagePassing):
         self.out_channels = out_channels
         self.use_attention = use_attention
 
+        self.residual = residual
         if self.use_attention:
             self.heads = heads
             self.concat = concat
@@ -388,7 +438,7 @@ class HypergraphConv(MessagePassing):
             hyperedge_weight = x.new_ones(num_edges)
 
         x = self.lin(x)
-
+        data_x = x
         alpha = None
         if self.use_attention:
             assert hyperedge_attr is not None
@@ -427,6 +477,8 @@ class HypergraphConv(MessagePassing):
         if self.bias is not None:
             out = out + self.bias
 
+        if self.residual:
+            out = out + data_x
         return out
 
 
@@ -444,7 +496,8 @@ class HypergraphDiagSheafConv(MessagePassing):
     r"""
     
     """
-    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', left_proj=None, norm=None,
+    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', 
+                left_proj=None, norm=None, residual = False,
                  **kwargs):
         kwargs.setdefault('aggr', 'add')
         super().__init__(flow='source_to_target', node_dim=0, **kwargs)
@@ -455,7 +508,8 @@ class HypergraphDiagSheafConv(MessagePassing):
         self.norm_type = norm_type
         self.left_proj = left_proj
         self.norm = norm
-        
+        self.residual = residual
+
         if self.left_proj:
             self.lin_left_proj = MLP(in_channels=d, 
                         hidden_channels=d,
@@ -492,33 +546,6 @@ class HypergraphDiagSheafConv(MessagePassing):
 
         zeros(self.bias)
 
-    def normalisation_matrices(self, x, hyperedge_index, alpha, num_nodes, num_edges, norm_type='degree_norm'):
-        #return D_inv and B_inv used to normalised the laplacian (/propagation)
-        if norm_type == 'degree_norm':
-            #normalise using node/hyperedge degrees D_e and D_v in the paper
-            D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*self.d) 
-            D = 1.0 / D
-            D[D == float("inf")] = 0
-
-            B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = 1.0 / B
-            B[B == float("inf")] = 0
-
-            return D, B
-
-        elif norm_type == 'block_norm':
-            #normalise using diag(HHT) and diag(HTH) <- this take into account the values predicted in H as oposed to 0/1 as in the degree
-            # this way of computing the normalisation tensor is only valid for diagonal sheaf
-            D = scatter_add(alpha*alpha, hyperedge_index[0], dim=0, dim_size=num_nodes*self.d)
-            D = 1.0 / D #can compute inverse like this because the matrix is diagonal
-            D[D == float("inf")] = 0
-
-            B = scatter_add(alpha*alpha, hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = 1.0 / B #can compute inverse like this because the matrix is diagonal
-            B[B == float("inf")] = 0
-            return D, B
-
-
     def forward(self, x: Tensor, hyperedge_index: Tensor,
                 alpha, 
                 num_nodes,
@@ -535,16 +562,26 @@ class HypergraphDiagSheafConv(MessagePassing):
             x = self.lin_left_proj(x)
             x = x.reshape(-1,num_nodes * self.d).t()
         x = self.lin(x)
-        D_inv, B_inv = self.normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, self.norm_type)
+        data_x = x
+
+        #depending on norm_type D^-1 or D^-1/2
+        D_inv, B_inv = normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, self.d, self.norm_type)
+
+
+        if self.norm_type in ['sym_degree_norm', 'sym_block_norm']:
+            # compute D^(-1/2) @ X
+            x = D_inv.unsqueeze(-1) * x
 
         out = self.propagate(hyperedge_index, x=x, norm=B_inv, alpha=alpha,
                              size=(num_nodes*self.d, num_edges*self.d))
+
         out = self.propagate(hyperedge_index.flip([0]), x=out, norm=D_inv,
                              alpha=alpha, size=(num_edges*self.d, num_nodes*self.d))
 
         if self.bias is not None:
             out = out + self.bias
-
+        if self.residual:
+            out = out + data_x
         return out
 
 
@@ -562,7 +599,8 @@ class HypergraphOrthoSheafConv(MessagePassing):
     r"""
     
     """
-    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', left_proj=None, norm=None,
+    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', 
+                left_proj=None, norm=None, residual = False,
                  **kwargs):
         kwargs.setdefault('aggr', 'add')
         super().__init__(flow='source_to_target', node_dim=0, **kwargs)
@@ -573,7 +611,17 @@ class HypergraphOrthoSheafConv(MessagePassing):
         self.norm_type=norm_type
         self.norm=norm
 
+        #for ortho matrix block <=> degree
+        if self.norm_type == 'block_norm':
+            self.norm_type = 'degree_norm'
+        elif self.norm_type == 'sym_block_norm':
+            self.norm_type = 'sym_degree_norm'
+        
+
+
         self.left_proj = left_proj
+        self.residual = residual
+
         if self.left_proj:
             self.lin_left_proj = MLP(in_channels=d, 
                         hidden_channels=d,
@@ -604,31 +652,6 @@ class HypergraphOrthoSheafConv(MessagePassing):
         self.lin.reset_parameters()
         zeros(self.bias)
 
-    def normalisation_matrices(self, x, hyperedge_index, alpha, num_nodes, num_edges, norm_type='degree_norm'):
-        #return D_inv and B_inv used to normalised the propagation
-        if norm_type == 'degree_norm':
-            # D = scatter_add(alpha, hyperedge_index[0], dim=0, dim_size=num_nodes*self.d)
-            D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*self.d) 
-            D = 1.0 / D
-            D[D == float("inf")] = 0
-
-            B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            # B = scatter_add(alpha, hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = 1.0 / B
-            B[B == float("inf")] = 0
-
-            return D, B
-        elif norm_type == 'block_norm':
-            # normalise using diag(HHT) and diag(HTH) <- this take into account the values predicted in H as oposed to 0/1 as in the degree
-            # For orthonormal blocks block_norm is equivalent to degree_norm (because of the HHT=I condition)s
-            D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*self.d) 
-            D = 1.0 / D
-            D[D == float("inf")] = 0
-
-            B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = 1.0 / B
-            B[B == float("inf")] = 0
-            return D, B
 
     def forward(self, x: Tensor, hyperedge_index: Tensor,
                 alpha, 
@@ -646,7 +669,13 @@ class HypergraphOrthoSheafConv(MessagePassing):
             x = self.lin_left_proj(x)
             x = x.reshape(-1,num_nodes * self.d).t()
         x = self.lin(x)    
-        D_inv, B_inv = self.normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, norm_type=self.norm_type)
+        data_x = x
+
+        D_inv, B_inv = normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, self.d, norm_type=self.norm_type)
+
+        if self.norm_type in ['sym_degree_norm', 'sym_block_norm']:
+            # compute D^(-1/2) @ X
+            x = D_inv.unsqueeze(-1) * x
 
         out = self.propagate(hyperedge_index, x=x, norm=B_inv, alpha=alpha,
                              size=(num_nodes*self.d, num_edges*self.d))
@@ -656,6 +685,8 @@ class HypergraphOrthoSheafConv(MessagePassing):
         if self.bias is not None:
             out = out + self.bias
 
+        if self.residual:
+            out = out + data_x
         return out
 
 
@@ -675,7 +706,8 @@ class HypergraphGeneralSheafConv(MessagePassing):
     r"""
     
     """
-    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', left_proj=None, norm=None,
+    def __init__(self, in_channels, out_channels, d, device, dropout=0, bias=True, norm_type='degree_norm', 
+                left_proj=None, norm=None, residual=False,
                  **kwargs):
         kwargs.setdefault('aggr', 'add')
         super().__init__(flow='source_to_target', node_dim=0, **kwargs)
@@ -686,7 +718,8 @@ class HypergraphGeneralSheafConv(MessagePassing):
         self.norm=norm
 
         self.left_proj = left_proj
-        
+        self.residual = residual
+
         if self.left_proj:
             self.lin_left_proj = MLP(in_channels=d, 
                         hidden_channels=d,
@@ -720,31 +753,42 @@ class HypergraphGeneralSheafConv(MessagePassing):
         self.lin.reset_parameters()
         zeros(self.bias)
 
-  
-    def normalisation_matrices(self, x, hyperedge_index, alpha, num_nodes, num_edges, norm_type='degree_norm'):
-        #return D_inv and B_inv used to normalised the propagation
-        if norm_type == 'degree_norm':
-            D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*self.d) 
-            D = 1.0 / D
-            D[D == float("inf")] = 0
+    def normalise(self, h_general_sheaf, hyperedge_index, norm_type, num_nodes, num_edges):
+        #this is just for block and sym_block normalisation
+        
+        #index correspond to the small matrix
+        row_small = hyperedge_index[0].view(-1,self.d,self.d)[:,0,0] // self.d
+        col_small = hyperedge_index[1].view(-1,self.d,self.d)[:,0,0] // self.d
 
-            B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = 1.0 / B
-            B[B == float("inf")] = 0
+        h_general_sheaf_1 = h_general_sheaf.reshape(row_small.shape[0], self.d, self.d)
 
-            return D, B
-        elif norm_type == 'block_norm':
-      
-            # D = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*self.d) 
-            D = x.new_ones(num_nodes*self.d)
-            # D[D == float("inf")] = 0
+        # 
+        to_be_inv_nodes = torch.bmm(h_general_sheaf_1, h_general_sheaf_1.permute(0,2,1)) 
+        to_be_inv_nodes = scatter_add(to_be_inv_nodes, row_small, dim=0, dim_size=num_nodes)
 
-            # B = scatter_add(x.new_ones(hyperedge_index.size(1)), hyperedge_index[1], dim=0, dim_size=num_edges*self.d)
-            B = x.new_ones(num_edges*self.d)
-            # B[B == float("inf")] = 0
+        # 
+        to_be_inv_edges = torch.bmm(h_general_sheaf_1.permute(0,2,1), h_general_sheaf_1)
+        to_be_inv_edges = scatter_add(to_be_inv_edges, col_small, dim=0, dim_size=num_edges)
 
-            return D, B
+        if norm_type in ['block_norm']:
+            d_inv_nodes = utils.batched_sym_matrix_pow(to_be_inv_nodes, -1.0) #n_nodes x d x d
+            d_inv_edges = utils.batched_sym_matrix_pow(to_be_inv_edges, -1.0) #n_edges x d x d
 
+            return d_inv_nodes, d_inv_edges
+
+        elif norm_type in ['sym_block_norm']:
+            d_sqrt_inv_nodes = utils.batched_sym_matrix_pow(to_be_inv_nodes, -0.5) #n_nodes x d x d
+            d_sqrt_inv_edges = utils.batched_sym_matrix_pow(to_be_inv_edges, -0.5) #n_edges x d x d
+
+            # alpha_norm = torch.bmm(d_sqrt_inv_nodes_large, h_general_sheaf_1)
+            # alpha_norm = torch.bmm(alpha_norm, d_sqrt_inv_edges_large)
+            # h_general_sheaf = alpha_norm.clamp(min=-1, max=1)
+            # h_general_sheaf = h_general_sheaf.reshape(h_general_sheaf.shape[0], self.d*self.d)
+            return d_sqrt_inv_nodes, d_sqrt_inv_edges
+
+        # ones_nodes_large = torch.ones((num_nodes,self.d, self.d)).to(h_general_sheaf.device)
+        # ones_edges_large = torch.ones((num_edges,self.d, self.d)).to(h_general_sheaf.device)
+        # return ones_nodes_large, ones_edges_large
 
     def forward(self, x: Tensor, hyperedge_index: Tensor,
                 alpha, 
@@ -764,15 +808,48 @@ class HypergraphGeneralSheafConv(MessagePassing):
             x = x.reshape(-1,num_nodes * self.d).t()
 
         x = self.lin(x)
-        D_inv, B_inv = self.normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, norm_type=self.norm_type)
+        data_x = x
+        if self.norm_type in ['block_norm', 'sym_block_norm']:
+            # NOTE: the normalisation is specific to general sheaf
+            D_inv, B_inv = self.normalise(alpha, hyperedge_index, self.norm_type, num_nodes, num_edges) # num_nodes x d x d
+            D_inv = torch.block_diag(*torch.unbind(D_inv)) # nd x nd
+            B_inv = torch.block_diag(*torch.unbind(B_inv)) # ed x ed
+            # D_inv = D_inv.clamp(-2,2)
+            # B_inv = B_inv.clamp(-2,2)
+
+        else:
+            D_inv, B_inv = normalisation_matrices(x, hyperedge_index, alpha, num_nodes, num_edges, self.d, norm_type=self.norm_type)
         
-        out = self.propagate(hyperedge_index, x=x, norm=B_inv, alpha=alpha,
-                            size=(num_nodes*self.d, num_edges*self.d))
-        out = self.propagate(hyperedge_index.flip([0]), x=out, norm=D_inv,
-                            alpha=alpha, size=(num_edges*self.d, num_nodes*self.d))
+        # x: (num_nodes*d) x f
+        if self.norm_type  == 'sym_degree_norm':
+            # compute D^(-1/2) @ X
+            x = D_inv.unsqueeze(-1) * x
+        elif self.norm_type  == 'sym_degree_norm':
+            x = D_inv @ x
+
+        if self.norm_type in ['sym_degree_norm', 'degree_norm']:
+            out = self.propagate(hyperedge_index, x=x, norm=B_inv, alpha=alpha,
+                                size=(num_nodes*self.d, num_edges*self.d))
+            out = self.propagate(hyperedge_index.flip([0]), x=out, norm=D_inv,
+                                alpha=alpha, size=(num_edges*self.d, num_nodes*self.d))
+        
+        elif self.norm_type in ['sym_block_norm', 'block_norm']:
+            #we keem norm as identity, but explicitly multiply with the corresponding matrix
+            no_norm_node = torch.ones(num_nodes*self.d).to(x.device)
+            no_norm_edge = torch.ones(num_edges*self.d).to(x.device)
+
+            out = self.propagate(hyperedge_index, x=x, norm=no_norm_edge, alpha=alpha,
+                                size=(num_nodes*self.d, num_edges*self.d))
+            out = B_inv @ out
+            out = self.propagate(hyperedge_index.flip([0]), x=out, norm=no_norm_node,
+                                alpha=alpha, size=(num_edges*self.d, num_nodes*self.d))
+            out = D_inv @ out 
         
         if self.bias is not None:
             out = out + self.bias
+
+        if self.residual:
+            out = out + data_x
 
         return out
 
@@ -819,6 +896,7 @@ class MLP(nn.Module):
                         nn.Linear(hidden_channels, hidden_channels))
                     self.normalizations.append(nn.BatchNorm1d(hidden_channels))
                 self.lins.append(nn.Linear(hidden_channels, out_channels))
+
         elif Normalization == 'ln':
             print("using LN")
             if num_layers == 1:

src/models.py
@@ -128,6 +128,8 @@ class SheafHyperGCN(nn.Module):
         self.hyperedge_attr = None
         self.dynamic_sheaf = args.dynamic_sheaf
 
+        self.residual = args.residual_HCHA
+        self.sheaf_type = sheaf_type
         # sheaf_type = 'OrthoSheafs'
         if sheaf_type == 'DiagSheafs':
             ModelSheaf = HGCNSheafBuilderDiag
@@ -185,25 +187,56 @@ class SheafHyperGCN(nn.Module):
     def normalise(self, A, hyperedge_index, num_nodes, d):
         if self.args.sheaf_normtype == 'degree_norm':
             D = scatter_add(hyperedge_index.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*d) 
+            D = torch.pow(D, -1.0)
+            D[D == float("inf")] = 0
+            D = torch.diag(D).to_sparse()
+            A = torch.sparse.mm(D, A) # this is laplacian delta
+
+        elif self.args.sheaf_normtype == 'sym_degree_norm':
+            D = scatter_add(hyperedge_index.new_ones(hyperedge_index.size(1)), hyperedge_index[0], dim=0, dim_size=num_nodes*d) 
             D = torch.pow(D, -0.5)
             D[D == float("inf")] = 0
             D = torch.diag(D).to_sparse()
             A = torch.sparse.mm(D, A) # this is laplacian delta
             A = torch.sparse.mm(A, D) # this is laplacian delta
 
-        # #TODO: check this. seems buggy
-        # elif self.args.sheaf_normtype == 'block_norm':
-        #     #normalise using diag(A) and diag(HTH) 
-        #     # this way of computing the normalisation tensor is ONLY valid for diagonal sheaf
-        #     D = A * torch.eye(A.shape[0]).to(A.device)
-        #     D = D.to_dense().diagonal()
-        #     D = torch.pow(D, -0.5) #can compute inverse like this because the matrix is diagonal
-        #     D = torch.nan_to_num(D, nan=0.0, posinf=0, neginf=0)
+        elif self.args.sheaf_normtype == 'block_norm':
+            # extract diagonals
+            D = A.to_dense().view((num_nodes, d, num_nodes, d))
+            D = torch.permute(D, (0,2,1,3)) #num_nodes x num_nodes x d x d
+            D = torch.diagonal(D, dim1=0, dim2=1) # d x d x num_nodes
+            D = torch.permute(D, (2,0,1)) #num_nodes x d x d
 
-        #     D = torch.diag(D).to_sparse()
-        #     A = torch.sparse.mm(D, A) # this is laplacian delta
-        #     A = torch.sparse.mm(A, D) # this is laplacian delta
-            
+            if self.sheaf_type in ["GeneralSheafs", "LowRankSheafs"]:
+                D = utils.batched_sym_matrix_pow(D, -1.0) #num_nodes x d x d
+            else:
+                D = torch.pow(D, -1.0)
+                D[D == float("inf")] = 0
+
+            D = torch.block_diag(*torch.unbind(D,0))
+            D = D.to_sparse()
+            A = torch.sparse.mm(D, A) # this is laplacian delta
+            if self.sheaf_type in ["GeneralSheafs", "LowRankSheafs"]:
+                A = A.to_dense().clamp(-1,1).to_sparse()
+        elif self.args.sheaf_normtype == 'sym_block_norm':
+            # extract diagonals
+            D = A.to_dense().view((num_nodes, d, num_nodes, d))
+            D = torch.permute(D, (0,2,1,3)) #num_nodes x num_nodes x d x d
+            D = torch.diagonal(D, dim1=0, dim2=1) # d x d x num_nodes
+            D = torch.permute(D, (2,0,1)) #num_nodes x d x d
+
+            if self.sheaf_type in ["GeneralSheafs", "LowRankSheafs"]:
+                D = utils.batched_sym_matrix_pow(D, -0.5) #num_nodes x d x d
+            else:
+                D = torch.pow(D, -0.5)
+                D[D == float("inf")] = 0
+
+            D = torch.block_diag(*torch.unbind(D,0))
+            D = D.to_sparse()
+            A = torch.sparse.mm(D, A) # this is laplacian delta
+            A = torch.sparse.mm(A, D) # this is laplacian delta
+            if self.sheaf_type in ["GeneralSheafs", "LowRankSheafs"]:
+                A = A.to_dense().clamp(-1,1).to_sparse()
         return A
 
     def forward(self, data):
@@ -405,6 +438,8 @@ class HyperSheafs(nn.Module):
         self.hyperedge_attr = None
         self.dynamic_sheaf = args.dynamic_sheaf
 
+        self.residual = args.residual_HCHA
+
         if args.cuda in [0, 1]:
             self.device = torch.device('cuda:'+str(args.cuda)
                               if torch.cuda.is_available() else 'cpu')
@@ -435,13 +470,17 @@ class HyperSheafs(nn.Module):
         
 #         Note that add dropout to attention is default in the original paper
         self.convs = nn.ModuleList()
-        self.convs.append(ModelConv(self.MLP_hidden, self.MLP_hidden, d=self.d, device=self.device, norm_type=self.norm_type, left_proj=self.left_proj, norm=self.norm))
+        self.convs.append(ModelConv(self.MLP_hidden, self.MLP_hidden, d=self.d, device=self.device, 
+                                        norm_type=self.norm_type, left_proj=self.left_proj, 
+                                        norm=self.norm, residual=self.residual))
         
         self.sheaf_builder = nn.ModuleList()
         self.sheaf_builder.append(ModelSheaf(args))
         #iulia Qs: add back the multi-layers?
         for _ in range(self.num_layers-1):
-            self.convs.append(ModelConv(self.MLP_hidden, self.MLP_hidden, d=self.d, device=self.device, norm_type=self.norm_type, left_proj=self.left_proj, norm=self.norm))
+            self.convs.append(ModelConv(self.MLP_hidden, self.MLP_hidden, d=self.d, device=self.device, 
+                                        norm_type=self.norm_type, left_proj=self.left_proj, 
+                                        norm=self.norm, residual=self.residual))
             if self.dynamic_sheaf:
                 self.sheaf_builder.append(ModelSheaf(args))
                 
@@ -580,7 +619,8 @@ class HCHA(nn.Module):
         self.init_hedge = args.init_hedge
         self.hyperedge_attr = None
 
-#         Note that add dropout to attention is default in the original paper
+        self.residual = args.residual_HCHA
+#        Note that add dropout to attention is default in the original paper
         self.convs = nn.ModuleList()
         #iulia Gs: should change here heads=args.heads?
         self.convs.append(HypergraphConv(args.num_features,
@@ -624,7 +664,7 @@ class HCHA(nn.Module):
 
         for i, conv in enumerate(self.convs[:-1]):
             # print(i)
-            x = F.elu(conv(x, edge_index, hyperedge_attr = self.hyperedge_attr))
+            x = F.elu(conv(x, edge_index, hyperedge_attr = self.hyperedge_attr, residual=self.residual))
             x = F.dropout(x, p=self.dropout, training=self.training)
 #         x = F.dropout(x, p=self.dropout, training=self.training)
 

src/sheaf_builder.py
@@ -226,6 +226,8 @@ class SheafBuilderGeneral(nn.Module):
             self.cp_W.reset_parameters()
             self.cp_V.reset_parameters()
 
+        
+
     def forward(self, x, e, hyperedge_index, debug=False):
         """ 
         x: N x f
@@ -260,6 +262,7 @@ class SheafBuilderGeneral(nn.Module):
         # h_general_sheaf = h_general_sheaf.reshape(-1,self.d*self.d)
         if debug:
             self.h_general_sheaf = h_general_sheaf #for debug purpose
+
         if self.sheaf_dropout:
             h_general_sheaf = F.dropout(h_general_sheaf, p=self.dropout, training=self.training)
         if (debug==True):
@@ -279,39 +282,13 @@ class SheafBuilderGeneral(nn.Module):
         d_range_edges = d_range.repeat(self.d).view(-1,1) #0,1..d,0,1..d..   d*d elems
         d_range_nodes = d_range.repeat_interleave(self.d).view(-1,1) #0,0..0,1,1..1..d,d..d  d*d elems
         hyperedge_index = hyperedge_index.unsqueeze(1) 
-   
+        # print(hyperedge_index)
 
         hyperedge_index_0 = self.d * hyperedge_index[0] + d_range_nodes
         hyperedge_index_0 = hyperedge_index_0.permute((1,0)).reshape(1,-1)
         hyperedge_index_1 = self.d * hyperedge_index[1] + d_range_edges
         hyperedge_index_1 = hyperedge_index_1.permute((1,0)).reshape(1,-1)
         h_general_sheaf_index = torch.concat((hyperedge_index_0, hyperedge_index_1), 0)
-
-        if self.norm_type == 'block_norm':
-            # pass
-            h_general_sheaf_1 = h_general_sheaf.reshape(h_general_sheaf.shape[0], self.d, self.d)
-            num_nodes = hyperedge_index[0].max().item() + 1
-            num_edges = hyperedge_index[1].max().item() + 1
-
-            to_be_inv_nodes = torch.bmm(h_general_sheaf_1, h_general_sheaf_1.permute(0,2,1)) 
-            to_be_inv_nodes = scatter_add(to_be_inv_nodes, row, dim=0, dim_size=num_nodes)
-
-            to_be_inv_edges = torch.bmm(h_general_sheaf_1.permute(0,2,1), h_general_sheaf_1)
-            to_be_inv_edges = scatter_add(to_be_inv_edges, col, dim=0, dim_size=num_edges)
-
-
-            d_sqrt_inv_nodes = utils.batched_sym_matrix_pow(to_be_inv_nodes, -1.0) #n_nodes x d x d
-            d_sqrt_inv_edges = utils.batched_sym_matrix_pow(to_be_inv_edges, -1.0) #n_edges x d x d
-            
-
-            d_sqrt_inv_nodes_large = torch.index_select(d_sqrt_inv_nodes, dim=0, index=row)
-            d_sqrt_inv_edges_large = torch.index_select(d_sqrt_inv_edges, dim=0, index=col)
-
-
-            alpha_norm = torch.bmm(d_sqrt_inv_nodes_large, h_general_sheaf_1)
-            alpha_norm = torch.bmm(alpha_norm, d_sqrt_inv_edges_large)
-            h_general_sheaf = alpha_norm.clamp(min=-1, max=1)
-            h_general_sheaf = h_general_sheaf.reshape(h_general_sheaf.shape[0], self.d*self.d)
 
         #!!! Is this the correct reshape??? Please check!!
         h_general_sheaf_attributes = h_general_sheaf.reshape(-1)
@@ -489,7 +466,6 @@ class SheafBuilderLowRank(nn.Module):
         self.norm_type = args.sheaf_normtype
 
         self.rank = args.rank # r for the block matrices
-        self.noisy_low_rank = args.noisy_low_rank
 
         if self.prediction_type == 'transformer':
             transformer_head = args.sheaf_transformer_head
@@ -501,7 +477,7 @@ class SheafBuilderLowRank(nn.Module):
             self.transformer_lin_layer = MLP(
                     in_channels=2*self.MLP_hidden, 
                     hidden_channels=args.MLP_hidden,
-                    out_channels=2*self.d*self.rank,
+                    out_channels=2*self.d*self.rank+self.d,
                     num_layers=1,
                     dropout=0.0,
                     Normalization='ln',
@@ -511,7 +487,7 @@ class SheafBuilderLowRank(nn.Module):
             self.general_sheaf_lin = MLP(
                     in_channels=2*self.MLP_hidden, 
                     hidden_channels=args.MLP_hidden,
-                    out_channels=2*self.d*self.rank,
+                    out_channels=2*self.d*self.rank+self.d,
                     num_layers=1,
                     dropout=0.0,
                     Normalization='ln',
@@ -586,16 +562,15 @@ class SheafBuilderLowRank(nn.Module):
 
         # h_general_sheaf is nnz x (2*d*r)
         h_general_sheaf_A = h_general_sheaf[:, :self.d*self.rank].reshape(h_general_sheaf.shape[0], self.d, self.rank) #nnz x d x r
-        h_general_sheaf_B = h_general_sheaf[:, self.d*self.rank:].reshape(h_general_sheaf.shape[0], self.d, self.rank) #nnz x d x r
+        h_general_sheaf_B = h_general_sheaf[:, self.d*self.rank:2*self.d*self.rank].reshape(h_general_sheaf.shape[0], self.d, self.rank) #nnz x d x r
+        h_general_sheaf_C = h_general_sheaf[:, 2*self.d*self.rank:].reshape(h_general_sheaf.shape[0], self.d) #nnz x d x r
+        
         h_general_sheaf = torch.bmm(h_general_sheaf_A,h_general_sheaf_B.transpose(2,1)) #rank-r matrix
         
-        if self.noisy_low_rank:
-            # torch.manual_seed(0)
-            std = 0.1
-            noise = torch.randn(self.d).to(h_general_sheaf.device) * std
-            noise = noise.unsqueeze(0).repeat((h_general_sheaf.shape[0],1))
-            noise = torch.diag_embed(noise)
-            h_general_sheaf = h_general_sheaf + noise
+        #add elements on the diagonal
+        diag = torch.diag_embed(h_general_sheaf_C)
+        h_general_sheaf = h_general_sheaf + diag
+
         h_general_sheaf = h_general_sheaf.reshape(h_general_sheaf.shape[0], self.d*self.d)
         
         if debug:
@@ -1184,7 +1159,6 @@ class HGCNSheafBuilderLowRank(nn.Module):
         self.norm = args.AllSet_input_norm
 
         self.rank = args.rank
-        self.noisy_low_rank = args.noisy_low_rank
 
         if self.prediction_type == 'transformer':
             transformer_head = args.sheaf_transformer_head
@@ -1206,7 +1180,7 @@ class HGCNSheafBuilderLowRank(nn.Module):
             self.sheaf_lin = MLP(
                         in_channels=self.MLP_hidden + args.MLP_hidden, 
                         hidden_channels=args.MLP_hidden,
-                        out_channels=2*self.d*self.rank,
+                        out_channels=2*self.d*self.rank+self.d,
                         num_layers=1,
                         dropout=0.0,
                         Normalization='ln',
@@ -1215,7 +1189,7 @@ class HGCNSheafBuilderLowRank(nn.Module):
             self.sheaf_lin = MLP(
                         in_channels=2*self.MLP_hidden, 
                         hidden_channels=args.MLP_hidden,
-                        out_channels=2*self.d*self.rank,
+                        out_channels=2*self.d*self.rank+self.d,
                         num_layers=1,
                         dropout=0.0,
                         Normalization='ln',
@@ -1290,16 +1264,13 @@ class HGCNSheafBuilderLowRank(nn.Module):
 
         # h_general_sheaf is nnz x (2*d*r)
         h_general_sheaf_A = h_sheaf[:, :self.d*self.rank].reshape(h_sheaf.shape[0], self.d, self.rank) #nnz x d x r
-        h_general_sheaf_B = h_sheaf[:, self.d*self.rank:].reshape(h_sheaf.shape[0], self.d, self.rank) #nnz x d x r
+        h_general_sheaf_B = h_sheaf[:, self.d*self.rank:2*self.d*self.rank].reshape(h_sheaf.shape[0], self.d, self.rank) #nnz x d x r
+        h_general_sheaf_C = h_sheaf[:, 2*self.d*self.rank:].reshape(h_sheaf.shape[0], self.d) #nnz x d x r
+
         h_sheaf = torch.bmm(h_general_sheaf_A,h_general_sheaf_B.transpose(2,1)) #rank-r matrix
 
-        #not sure if adding randomness is ideal but let's try
-        if self.noisy_low_rank:
-            std = 0.1
-            noise = torch.randn(self.d).to(h_sheaf.device) * std
-            noise = noise.unsqueeze(0).repeat((h_sheaf.shape[0],1))
-            noise = torch.diag_embed(noise)
-            h_sheaf = h_sheaf + noise
+        diag = torch.diag_embed(h_general_sheaf_C)
+        h_sheaf = h_sheaf + diag
 
         h_sheaf = h_sheaf.reshape(h_sheaf.shape[0], self.d*self.d)
 

src/test_sheaf_conv.py
@@ -26,10 +26,10 @@ def get_test_config():
         'num_features': 16,
         'num_classes': 32,
         'heads': 4,
-        'sheaf_normtype': 'degree_norm',
-        'sheaf_pred_block': 'cp_decomp',
+        'sheaf_normtype': 'block_norm',
+        'sheaf_pred_block': 'MLP_var1',
         'sheaf_transformer_head': 8,#MLP_hidden needs to divide this
-        'dynamic_sheaf': True,
+        'dynamic_sheaf': False,
         'sheaf_left_proj': False,
         'sheaf_act': 'sigmoid',
         'sheaf_dropout': False,
@@ -50,7 +50,7 @@ def get_test_config():
         'HyperGCN_mediators': True,
 
         'rank': 2,
-        'noisy_low_rank': True
+        'residual_HCHA': False
 
 
     }
@@ -248,7 +248,7 @@ def test_equivariance_sheaf_general():
 
         print(out.mean(), perm_out.mean())
         print(torch.abs(out-perm_out).max())
-        assert(torch.allclose(out, perm_out, atol=1e-5))
+        assert(torch.allclose(out, perm_out, atol=1e-4))
         print("model GeneralSheafs is permutation equivariant")
 
 def test_equivariance_sheaf_low_rank():
@@ -397,7 +397,7 @@ def test_equivariance_sheaf_hgcn(sheaf_type):
         # print("perm_out", perm_out.mean(-1))
 
         print(torch.abs(out-perm_out).max())
-        # print(out.mean(), perm_out.mean())
+        print(out.mean(), perm_out.mean())
         assert(torch.allclose(out, perm_out, atol=1e-5))
         print(f"model HGCN {sheaf_type} is permutation equivariant")
 